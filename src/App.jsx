a2d713170719435499cebcfba0a32a74befa9fb2
Dmitriy Egorov <dmitriy.v.egorov@gmail.com>
2025-02-04 12:38:12 +0400

    first commit before splitting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './components/Recharts.jsx'
+import Recharts from './components/Recharts.jsx'
+import './ResponsiveAppBar.jsx'
+import ResponsiveAppBar from './ResponsiveAppBar.jsx'
+
+function App() {
+  
+  return (
+    <>
+    <ResponsiveAppBar />
+      <Recharts />
+    </>
+  )
+}
+
+export default App
